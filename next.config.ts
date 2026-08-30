26e471a73eda0dbf6e0a56f9284d32465d878ed4
semshang <semshang@example.com>
2026-08-30 16:00:03 +0545

    fix: Configure lh3.googleusercontent.com in next.config for next/image

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-﻿import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
   async headers() {
     return [
       {
